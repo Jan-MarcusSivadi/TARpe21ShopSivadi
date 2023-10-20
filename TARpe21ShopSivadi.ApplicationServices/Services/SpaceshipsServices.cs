bf34efae11494f4a8399de6ed297eecc4d0469c0
Jan-MarcusSivadi <jmarcus.sivadi@gmail.com>
2023-10-20 19:25:33 +0300

    added delete/details views, update ViewData[Title] for all views + update services and controllers

--- TARpe21ShopSivadi.ApplicationServices/Services/SpaceshipsServices.cs
+++ TARpe21ShopSivadi.ApplicationServices/Services/SpaceshipsServices.cs
@@ -51,31 +51,55 @@ namespace TARpe21ShopSivadi.ApplicationServices.Services
         }
         public async Task<Spaceship> Update(SpaceshipDto dto)
         {
-            var domain = new Spaceship()
-            {
-                Name = dto.Name,
-                Description = dto.Description,
-                //Dimensions = dto.Dimensions,
-                PassengerCount = dto.PassengerCount,
-                CrewCount = dto.CrewCount,
-                CargoWeight = dto.CargoWeight,
-                MaxSpeedInVaccuum = dto.MaxSpeedInVaccuum,
-                BuiltAtDate = dto.BuiltAtDate,
-                MaidenLaunch = dto.MaidenLaunch,
-                Manufacturer = dto.Manufacturer,
-                IsSpaceshipPreviouslyOwned = dto.IsSpaceshipPreviouslyOwned,
-                FullTripsCount = dto.FullTripsCount,
-                Type = dto.Type,
-                EnginePower = dto.EnginePower,
-                FuelConsumptionPerDay = dto.FuelConsumptionPerDay,
-                MaintenanceCount = dto.MaintenanceCount,
-                LastMaintenance = dto.LastMaintenance,
-                CreatedAt = dto.CreatedAt,
-                ModifiedAt = DateTime.Now,
-            };
-            _context.Spaceships.Update(domain);
+
+            //var domain = new Spaceship()
+            //{
+            //    Name = dto.Name,
+            //    Description = dto.Description,
+            //    //Dimensions = dto.Dimensions,
+            //    PassengerCount = dto.PassengerCount,
+            //    CrewCount = dto.CrewCount,
+            //    CargoWeight = dto.CargoWeight,
+            //    MaxSpeedInVaccuum = dto.MaxSpeedInVaccuum,
+            //    BuiltAtDate = dto.BuiltAtDate,
+            //    MaidenLaunch = dto.MaidenLaunch,
+            //    Manufacturer = dto.Manufacturer,
+            //    IsSpaceshipPreviouslyOwned = dto.IsSpaceshipPreviouslyOwned,
+            //    FullTripsCount = dto.FullTripsCount,
+            //    Type = dto.Type,
+            //    EnginePower = dto.EnginePower,
+            //    FuelConsumptionPerDay = dto.FuelConsumptionPerDay,
+            //    MaintenanceCount = dto.MaintenanceCount,
+            //    LastMaintenance = dto.LastMaintenance,
+            //    CreatedAt = dto.CreatedAt,
+            //    ModifiedAt = DateTime.Now,
+            //};
+            var spaceshipId = await _context.Spaceships
+                .FirstOrDefaultAsync(x => x.Id == dto.Id);
+
+            // Update model fields based on SpaceshipDto
+            spaceshipId.Name = dto.Name;
+            spaceshipId.Description = dto.Description;
+            spaceshipId.PassengerCount = dto.PassengerCount;
+            spaceshipId.CrewCount = dto.CrewCount;
+            spaceshipId.CargoWeight = dto.CargoWeight;
+            spaceshipId.MaxSpeedInVaccuum = dto.MaxSpeedInVaccuum;
+            spaceshipId.BuiltAtDate = dto.BuiltAtDate;
+            spaceshipId.MaidenLaunch = dto.MaidenLaunch;
+            spaceshipId.Manufacturer = dto.Manufacturer;
+            spaceshipId.IsSpaceshipPreviouslyOwned = dto.IsSpaceshipPreviouslyOwned;
+            spaceshipId.FullTripsCount = dto.FullTripsCount;
+            spaceshipId.Type = dto.Type;
+            spaceshipId.EnginePower = dto.EnginePower;
+            spaceshipId.FuelConsumptionPerDay = dto.FuelConsumptionPerDay;
+            spaceshipId.MaintenanceCount = dto.MaintenanceCount;
+            spaceshipId.LastMaintenance = dto.LastMaintenance;
+            spaceshipId.CreatedAt = dto.CreatedAt;
+            spaceshipId.ModifiedAt = DateTime.Now;
+
+            _context.Spaceships.Update(spaceshipId);
             await _context.SaveChangesAsync();
-            return domain;
+            return spaceshipId;
         }
         public async Task<Spaceship> GetUpdate(Guid id)
         {
